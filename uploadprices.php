19e2ac43d2e62aee2662a3f22d7a37375e9e894e
Areso <returnstrike@yandex.ru>
2018-01-20 17:59:01 +0600

    MSRP, DEALER and FLASH SALE prices upload

--- uploadprices.php
+++ uploadprices.php
@@ -8,46 +8,24 @@ ini_set("default_charset",'utf-8');//utf-8 windows-1251
 ini_set('display_errors', 1);
 set_time_limit(600);
 //get variables from input
-$csv1 = $_FILES["fileToUpload"]["name"];
-$man_id = $_POST["manufacturer_id"];
-//echo $man_id;
-//echo $csv1;
-//echo " isset ";
-//echo file($csv1);
+$csv1    = $_FILES["fileToUpload"]["name"];
+$man_id  = $_POST["manufacturer_id"];
+$scenary = $_POST["radio"];
+if ($scenary == "SPECIAL") {
+	$date_start = $_POST["date_start"];
+	$date_end   = $_POST["date_end"]; 
+};
 $csv2 = $_POST["name"];
 //$csv3 = array_map('str_getcsv', file('data.csv'));
 $csv3 = array_map('str_getcsv', file($_FILES["fileToUpload"]["tmp_name"]));
 $csv4 = str_getcsv(file($csv1), ';');
-if (isset($csv4)) {
-	//echo "csv4 str_getcsv map is set"; 
-	//echo "<br>";
-	//echo $csv4;
-} else {
-	//echo "csv4 str_getcsv map is not set";
-}
-//echo " size of ";
-//echo sizeof($csv4);
-foreach ($csv4 as $value) {
-	//echo "we are people";    
-	//echo $value;
-}
-//echo "<br>";
 $csvFile  = file_get_contents($_FILES["fileToUpload"][tmp_name]);
 $csvFile2 = explode("\n", file_get_contents($_FILES["fileToUpload"][tmp_name]));
 $csvFile2Len = sizeof($csvFile2);
-//echo $csvFile2Len;
-/*
-for ($i = 1; $i < $csvFile2Len; $i++) {
-	$acctArr[i-1] = str_getcsv($csvFile2, ";");
-	echo $acctArr[i-1]." "; 
-}
-*/
-//myvar = "a";
 $it = 0;
 $idArr    = Array();
 $priceArr = Array();
 foreach ($csvFile2 as $entry) {
-	
     $acctArr       = str_getcsv($entry, ";");
 	$idArr[$it]    = $acctArr[0];
 	$priceArr[$it] = $acctArr[1];
@@ -55,14 +33,8 @@ foreach ($csvFile2 as $entry) {
 	$it = $it+1;
 };
 echo " array size is ".$it;
-for ($i = 0; $i < $it; $i++) {
-	echo $idArr[$i]." ".$priceArr[$i]."<br>"; 
-};
 
-//echo " php. ";
-//echo $csvFile;
-//echo $csv3;
-//connect to database
+
 $conn = mysqli_connect(DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
 if (mysqli_connect_errno()) {
     die("Failed to connect to MySQL: " . mysqli_connect_error());
@@ -74,22 +46,51 @@ if (!mysqli_set_charset($conn, "utf8")) {
 } else {
   //  printf("Current character set: %s\n", mysqli_character_set_name($conn));
 }
-for ($i = 0; $i < $it; $i++) {
-	$query_line = "UPDATE oc_product 
- 	SET oc_product.price = ".$priceArr[$i].
- 	" WHERE (oc_product.manufacturer_id = ".$man_id.
- 	" && oc_product.model = ".$idArr[$i].
- 	")";
-	//echo "<br>";
-	//echo "hello";
-	echo $query_line;
-	mysqli_query($conn, $query_line);
-	echo "<br>";
-	echo "query result is ".$query;
-	//$result = $conn->query($query_line);
-	//echo $idArr[$i]." ".$priceArr[$i]."<br>"; 
+if ($scenary == "PRODUCT") { 
+	for ($i = 0; $i < $it; $i++) {
+		$query_line = "UPDATE oc_product 
+		SET oc_product.price = ".$priceArr[$i].
+		" WHERE (oc_product.manufacturer_id = ".$man_id.
+		" && oc_product.model = ".$idArr[$i].
+		")";
+		echo $query_line;
+		$result = mysqli_query($conn, $query_line);
+		echo " Query result is ".$result;
+		echo "<br>";		
+	};
 };
+if ($scenary == "DISCOUNT") {
+	for ($i = 0; $i < $it; $i++) {
+		$query_line = "UPDATE oc_product, oc_product_discount 
+		SET oc_product_discount.price = ".$priceArr[$i].
+		" WHERE (oc_product.product_id = oc_product_discount.product_id 
+		  && oc_product.manufacturer_id = ".$man_id.
+		" && oc_product.model = ".$idArr[$i].
+		")";
+		echo $query_line;
+		$result = mysqli_query($conn, $query_line);
+		echo " Query result is ".$result;
+		echo "<br>";		 
+	};
+};
+//INSERT INTO Customers (CustomerName, City, Country)
+//VALUES ((SELECT product_id FROM MyTable), 'Stavanger', 'Norway');
 
+if ($scenary == "SPECIAL") {
+	for ($i = 0; $i < $it; $i++) {
+		$query_line = "INSERT INTO oc_product_special 
+		(product_id, customer_group_id, priority, price, date_start, date_end) 
+		VALUES 
+		((SELECT oc_product.product_id FROM oc_product WHERE oc_product.model = ".$idArr[$i]." && oc_product.manufacturer_id = ".$man_id."), 
+		'2', '0', ".$priceArr[$i].", '".$date_start."', '".$date_end."')";
+		echo $query_line;
+		$result = mysqli_query($conn, $query_line);
+		echo " Query result is ".$result;
+		echo "<br>";
+		//echo "date start is ".$date_start." and date end is ".$date_end;
+		echo "<br>";
+	};	
+};
 echo "<html><body>";
 $query = mysqli_query($conn, $query_line);
 echo "<a href=\"uploadprices.html\">back to input</a>";
